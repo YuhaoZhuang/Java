2c5ae3d6b82a52cc9bf571a2d8e76804fcb3184e
YuhaoZhuang <zhuangy625@gmail.com>
2016-09-08 11:53:57 -0700

    addeed lesson 1

---
+++ Lesson_01/firstProg.java
@@ -0,0 +1,27 @@
+public class firstProg 
+{
+
+	public static void main(String[]args)
+	{
+		System.out.println ("  Yuhao	  ");    
+  System.out.println ("  Zhuang	 ");  
+  System.out.println ("  111 ");  
+  System.out.println ("  Tryin hard     ");
+  System.out.println ("  To surive    ");
+  System.out.println ("<<<<<>>>>>>");
+  System.out.println ("   $$$$$    ");      
+  System.out.println ("   ||###      ");     
+  System.out.println ("   QQQQQ       ");  
+  System.out.println ("   IIIII      ");  
+  System.out.println ("   777777      "); 
+  System.out.println ("   ||_||      ");
+  System.out.println ("   THEEEEE     ");
+  System.out.println ("|~~~`-`~~~|   ");
+  System.out.println ("|         |    ");
+  System.out.println ("|____Million_____|    ");
+  System.out.println ("|_________|    ");
+  System.out.println ("    -| ||       ");
+  System.out.println ("    |_||__        ");
+  System.out.println ("    ((____))    ");
+	}
+}
---
+++ Lesson_01/firstProg1.java
@@ -0,0 +1,27 @@
+public class firstProg1 
+{
+
+	public static void main(String[]args)
+	{
+		System.out.println ("  Yuhao	  ");    
+  System.out.println ("  Zhuang	 ");  
+  System.out.println ("  111 ");  
+  System.out.println ("  Trying hard     ");
+  System.out.println ("  To surive    ");
+  System.out.println ("<<<<<>>>>>>");
+  System.out.println ("  Graceful   ");      
+  System.out.println ("   ||###2222      ");     
+  System.out.println ("   SSHHHHH     ");  
+  System.out.println ("   <>HAHAHAH    ");  
+  System.out.println ("   777777      "); 
+  System.out.println ("   ||_||      ");
+  System.out.println ("   WHEEEE  ");
+  System.out.println ("|~~~`-`~~~|   ");
+  System.out.println ("|  RRR$$$$RRRR       |    ");
+  System.out.println ("|____Million_____|    ");
+  System.out.println ("LEEEEEEE  ");
+  System.out.println ("    | ||       ");
+  System.out.println ("    |_||__        ");
+  System.out.println ("   HIIIIITTTTT  ");
+	}
+}
