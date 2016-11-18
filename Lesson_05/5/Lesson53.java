460b8e3847f176f6c31fb4fe4919d0909349245a
YuhaoZhuang <zhuangy625@gmail.com>
2016-11-18 11:42:55 -0800

    update

---
+++ Lesson_05/5/Lesson53.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+public class Lesson53
+{
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		String a = "";
+		System.out.println("BMI calculator!");
+		System.out.println("Please enter your weight in pounds.");
+		double weight = kb.nextDouble();
+		System.out.println("Please enther your height in inches.");
+		double height = kb.nextDouble();
+		double b = 703 * weight / height / height; 
+		if (b <= 18.5)
+			a= " underweight.";
+		else if (b <= 24.9)
+			a = " normal.";
+		else if (b <= 29.9)
+			a = " overweight.";
+		else if (b <= 34.9)
+			a = " obese.";
+		else if (b <= 39.9)
+			a = " very obese.";
+		else
+			a = " morbidly obese.";
+		
+		System.out.println("Your BMI is " + b + ".");
+		System.out.println("You are" + a);
+	}
+	
+}
