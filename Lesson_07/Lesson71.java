b433ae18f487d1c82988f0f9870f93e5a4e91525
YuhaoZhuang <zhuangy625@gmail.com>
2016-12-15 11:07:20 -0800

    added

--- Lesson_07/Lesson71.java
+++ Lesson_07/Lesson71.java
@@ -1,27 +1,31 @@
 import java.util.Scanner;
-public class Lesson71
 
+public class Lesson71
 {
-	static int number;
+	static int number = 234;
+	static double average = 0;
 	static int digits = 0;
-	static int average = 0;
+	
 	public static void main(String[]args)
 	{
-	   Scanner kb = new Scanner(System.in);
-	   System.out.println("Please enter your number: ");
-	   number = kb.nextInt();
-	   avDigits();
-	   System.out.println("The average of the digits in " + number + " is " + average);
+		Scanner kb = new Scanner(System.in);
+		avgDigits();
+		
+		System.out.println("The avg of the digits in " + number + " is " + average);
 	}
-	public static void avDigits()
+	
+	public static void avgDigits()
 	{
 		int num = number;
-		while( num > 0)
-		{ 
-	        digits ++;
+		
+		while (num > 0)
+		{
+			digits ++;
 			average += num % 10;
 			num /= 10;
-			int average = number / digits;
 		}
+		
+		average /= digits;
 	}
+	
 }
